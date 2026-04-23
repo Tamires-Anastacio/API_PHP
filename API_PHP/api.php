8dc27a7c3b3e4c9c22ffa0babb2e426543f234a9
Tamires-Anastacio <tamiresanastaciodefreitas283@gmail.com>
2026-04-23 08:11:33 -0300

    endpoint

--- API_PHP/api.php
+++ API_PHP/api.php
@@ -1,5 +1,5 @@
 <?php
-    header("Content-Type: application/json");
+    header("Content-Type: application/json; charset=UTF-8");
 
     $metodo = $_SERVER['REQUEST_METHOD'];
 
@@ -22,13 +22,20 @@
     switch ($metodo) {
         case 'GET':
             //echo "AQUI AÇÕES DO MÉTODO GET";
-            echo json_encode($usuarios);
+            echo json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
             break;
 
         case 'POST':
             //echo "AQUI AÇÕES DO MÉTODO POST";
             $dados = json_decode(file_get_contents('php://input'),true);
             //print_r($dados);
+
+
+        if (!isset($dados["id"]) || !isset($dados["nome"]) || !isset($dados["email"]))
+            http_response_code(400);
+            echo json_encode(["erro" => "Dados incompletos."], JSON_UNESCAPED_UNICODE);
+            exit;
+
 
             $novo_usuario = [
                 "id" => $dados["id"],
@@ -37,17 +44,25 @@
 
             ];
 
-            array_push($usuarios, $novo_usuario);
-            echo json_encode('Usuário inserido com sucesso!');
-            print_r($usuarios);
+            $usuarios[] = $novo_usuario;
+
+            file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+            break;
+
+            //array_push($usuarios, $novo_usuario);
+            //echo json_encode('Usuário inserido com sucesso!');
+            //print_r($usuarios);
 
             break;
         
-        default:
-            echo "MÉTODO NÃO ENCONTRADO!";
+            default:
+            //echo "MÉTODO NÃO ENCONTRADO!";
+            //break;
+            http_response_code(405);
+            echo json_encode(["erro" => "Método não permitido!"], JSON_UNESCAPED_UNICODE);
             break;
-    }
-
+    
+}
     
 
     
